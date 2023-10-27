e1cb2dc19ef34343ecf101b4a39f7ea87d1ac4ab
Zabolotnyi_Artem <zaaabik@gmail.com>
2023-10-27 14:56:52 +0400

    increase timeout value

--- api/language_model.py
+++ api/language_model.py
@@ -4,10 +4,13 @@ HEADERS = {"Accept": "application/json"}
 
 
 class LanguageModelAPI:
+    """Class contains methods to work with LLM generation api."""
+
     def __init__(self, base_api_path):
         self.base_api_path = base_api_path
 
     def generate(self, text: str):
+        """Function will call LLM api and return answer for user :str text: object."""
         path = f"{self.base_api_path}/generate"
-        answer = requests.post(url=path, json={"text": text}, timeout=10)
+        answer = requests.post(url=path, json={"text": text}, timeout=180)
         return answer.json()["generated_text"]
